fd76de520daf41068fae526baae415844d72f6eb
Alon Mardi <alon.mardi@gentex.com>
2024-03-23 12:12:48 +0200

    added package arch

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="research_utils",  # Replace with your own username
+    version="0.0.1",
+    author="Alon Mardi",
+    author_email="alonmardi@mail.tau.ac.il",
+    description="Generic utils package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Linux",
+    ],
+    python_requires='>=3.6',
+)
